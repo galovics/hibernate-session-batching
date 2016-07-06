25a97d33efb60bc0758b225f9821a960dcec1d5c
Arnold Galovics <galovicsarnold@gmail.com>
2016-07-06 22:13:00 +0200

    Removed reflection usage for setting back JDBC Batch size

--- src/main/java/com/arnoldgalovics/blog/batch/JdbcBatchSizeMethodInterceptor.java
+++ src/main/java/com/arnoldgalovics/blog/batch/JdbcBatchSizeMethodInterceptor.java
@@ -1,17 +1,13 @@
 package com.arnoldgalovics.blog.batch;
 
-import java.lang.reflect.Field;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.hibernate.Session;
-import org.hibernate.internal.AbstractSharedSessionContract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.ReflectionUtils;
 
 public class JdbcBatchSizeMethodInterceptor implements MethodInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(JdbcBatchSizeMethodInterceptor.class);
@@ -29,13 +25,7 @@ public class JdbcBatchSizeMethodInterceptor implements MethodInterceptor {
             result = invocation.proceed();
             entityManager.flush();
         } finally {
-            try {
-                final Field field = AbstractSharedSessionContract.class.getDeclaredField("jdbcBatchSize");
-                field.setAccessible(true);
-                ReflectionUtils.setField(field, session, originalBatchSize);
-            } catch (final Exception e) {
-                logger.error("Error when setting original jdbc batch size for the session.", e);
-            }
+            session.setJdbcBatchSize(originalBatchSize);
         }
         return result;
     }
